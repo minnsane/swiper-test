8ee91c08a2c5a76127252c1c465e3886a84248bb
minnsane <minnsane.kim@gmail.com>
2023-02-20 16:39:57 +0900

    feat: 기본 스와이프 cd 항상 감지 생성

--- src/app/default/default-cd-always/default-cd-always.component.ts
+++ src/app/default/default-cd-always/default-cd-always.component.ts
@@ -1,7 +1,38 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import Swiper, { SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-default-cd-always',
   templateUrl: './default-cd-always.component.html',
 })
-export class DefaultCdAlwaysComponent {}
+export class DefaultCdAlwaysComponent {
+  numbers = [1, 2, 3, 4, 5];
+
+  options: SwiperOptions = {
+    slidesPerView: 1,
+    initialSlide: Math.floor(Math.random() * this.numbers.length),
+    pagination: {
+      type: 'custom',
+      renderCustom: (swiper: Swiper, _current: number, total: number) =>
+        `<span>${swiper.realIndex + 1}</span> / <span>${total}</span>`,
+    },
+  };
+
+  addEvents(swiper: Swiper): void {
+    swiper.on('destroy', () => console.log('swiper destroyed'));
+    swiper.on('realIndexChange', (swiper) =>
+      console.log('swiper index: ', swiper.realIndex)
+    );
+  }
+
+  changePanelContents(): void {
+    this.numbers = this.numbers.map((v) => v * 2);
+  }
+
+  changePanelCount(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    const count = Number(target.value);
+
+    this.numbers = Array.from({ length: count }, (_, i) => i + 1);
+  }
+}
