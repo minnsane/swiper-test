20dad834c750f54064fae976f1d6a8458c61e396
minnsane <minnsane.kim@gmail.com>
2023-02-20 20:10:41 +0900

    feat: 회전 스와이퍼 일괄삭제 추가

--- src/app/loop/loop-rxjs-remove/loop-rxjs-remove.component.ts
+++ src/app/loop/loop-rxjs-remove/loop-rxjs-remove.component.ts
@@ -1,8 +1,54 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { SwiperContainerDirective } from 'src/app/swiper-container.directive';
+import Swiper, { SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-loop-rxjs-remove',
   templateUrl: './loop-rxjs-remove.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoopRxjsRemoveComponent {}
+export class LoopRxjsRemoveComponent {
+  @ViewChild(SwiperContainerDirective)
+  swiperContainer?: SwiperContainerDirective;
+
+  numbers$ = new BehaviorSubject<number[]>([1, 2, 3, 4, 5]);
+
+  options: SwiperOptions = {
+    loop: true,
+    slidesPerView: 1,
+    initialSlide: Math.floor(Math.random() * this.numbers$.value.length),
+    pagination: {
+      type: 'custom',
+      renderCustom: (swiper: Swiper, _current: number, total: number) =>
+        `<span>${swiper.realIndex + 1}</span> / <span>${total}</span>`,
+    },
+  };
+
+  addEvents(swiper: Swiper): void {
+    swiper.on('destroy', () => console.log('swiper destroyed'));
+    swiper.on('realIndexChange', (swiper) =>
+      console.log('swiper index: ', swiper.realIndex)
+    );
+    swiper.on('update', () => console.log('swiper updated'));
+  }
+
+  changePanelContentsStream(): void {
+    const nums = this.numbers$.value;
+
+    this.changeNumStream(nums.map((v) => v * 2));
+  }
+
+  changePanelCountStream(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    const count = Number(target.value);
+
+    this.changeNumStream(Array.from({ length: count }, (_, i) => i + 1));
+  }
+
+  private changeNumStream(numbers: number[]): void {
+    this.swiperContainer?.swiper.removeAllSlides();
+    this.numbers$.next(numbers);
+    setTimeout(() => this.swiperContainer?.swiper.update(), 0);
+  }
+}
