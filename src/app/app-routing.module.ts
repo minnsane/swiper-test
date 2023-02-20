f2e0ed2111c3ec8301a0c05600bae395859234d6
minnsane <minnsane.kim@gmail.com>
2023-02-20 17:45:04 +0900

    feat: 기본 스와이프 sticky top 적용

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DefaultCdAlwaysComponent } from './default/default-cd-always/default-cd-always.component';
 import { DefaultOnPushComponent } from './default/default-on-push/default-on-push.component';
+import { DefaultRxjsStickyComponent } from './default/default-rxjs-sticky/default-rxjs-sticky.component';
 import { DefaultRxjsComponent } from './default/default-rxjs/default-rxjs.component';
 
 const routes: Routes = [
   { path: 'default/cd-always', component: DefaultCdAlwaysComponent },
   { path: 'default/on-push', component: DefaultOnPushComponent },
   { path: 'default/rxjs', component: DefaultRxjsComponent },
+  { path: 'default/rxjs-sticky', component: DefaultRxjsStickyComponent },
 ];
 
 @NgModule({
--- src/app/default/default-rxjs-sticky/default-rxjs-sticky.component.ts
+++ src/app/default/default-rxjs-sticky/default-rxjs-sticky.component.ts
@@ -1,8 +1,41 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import Swiper, { SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-default-rxjs-sticky',
   templateUrl: './default-rxjs-sticky.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DefaultRxjsStickyComponent {}
+export class DefaultRxjsStickyComponent {
+  numbers$ = new BehaviorSubject<number[]>([1, 2, 3, 4, 5]);
+  isSticky = false;
+
+  options: SwiperOptions = {
+    slidesPerView: 1,
+    pagination: {
+      type: 'bullets',
+    },
+    navigation: true,
+  };
+
+  addEvents(swiper: Swiper): void {
+    swiper.on('destroy', () => console.log('swiper destroyed'));
+    swiper.on('realIndexChange', (swiper) =>
+      console.log('swiper index: ', swiper.realIndex)
+    );
+  }
+
+  changePanelContentsStream(): void {
+    const nums = this.numbers$.value;
+
+    this.numbers$.next(nums.map((v) => v * 2));
+  }
+
+  changePanelCountStream(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    const count = Number(target.value);
+
+    this.numbers$.next(Array.from({ length: count }, (_, i) => i + 1));
+  }
+}
